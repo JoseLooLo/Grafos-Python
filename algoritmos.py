196e4a9362129f37e892250ed66a7ee50410a735
Jose LooLo <joseloolo@hotmail.com>
2021-07-25 14:00:40 -0300

    euleriano

--- algoritmos.py
+++ algoritmos.py
@@ -2,7 +2,57 @@ from graph import Graph
 from collections import deque
 
 def euleriano(g):
-    pass
+    c = {}
+    for i in range(1,g.getNumberOfVertices()+1):
+        for j in g.vList[i].getNeighbors():
+            a = (i,j)
+            c[a] = False
+
+    r, ciclo = subEuleriano(g, valid(g), c)
+    if r:
+        print("1")
+        print(ciclo)
+    else:
+        print("0")
+
+def subEuleriano(g, vertex_v, c):
+    ciclo = []
+    ciclo.append(vertex_v)
+    t = vertex_v
+    while True:
+        for i in g.vList[t].getNeighbors():
+            a = (i,t)
+            b = (t,i)
+            if not c[a]:
+                c[a] = True
+                c[b] = True
+                t = i
+                ciclo.append(t)
+                break
+        else:
+            return False,None
+        if vertex_v == t:
+            break
+
+    for i in ciclo:
+        for j in g.vList[i].getNeighbors():
+            a = (i,j)
+            if not c[a]:
+                r, ciclo2 = subEuleriano(g, i, c)
+                if not r:
+                    return False, None
+                index = ciclo.index(i)
+                del ciclo[index]
+                for i in range(len(ciclo2)):
+                    ciclo.insert(index+i, ciclo2[i])
+
+    return True, ciclo
+
+def valid(g):
+    for i in range(1,g.getNumberOfVertices()+1):
+        if len(g.vList[i].getNeighbors()) > 0:
+            return i
+
 
 
 def bbs(filename, vertex_v):
--- main.py
+++ main.py
@@ -1,15 +1,16 @@
 from graph import Graph
-from algoritmos import bbs
+from algoritmos import bbs, euleriano
 #https://wiki.python.org/moin/TimeComplexity
 
 
 #bbs("karate.net", 1)
-a = Graph("ContemCicloEuleriano.net")
-a.euleriano()
+a = Graph("ex/ContemCicloEuleriano.net")
+#euleriano(a)
+#a.euleriano()
 #print(a.getNumberOfVertices())
 #print(a.getNumberOfEdges())
 #print(a.getNeighbors(8))
 #print(a.getNeighbors(135))
 #print(a.hasEdge(1,135))
 #print(a.getWeight(1,134))
-a.printBBS(1)
+#a.printBBS(1)
